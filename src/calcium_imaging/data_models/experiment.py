d558a4df9765976d40675ea69b0d337e67c1f96e
Eldar Abraham <eldar.abraham@gmail.com>
2025-04-22 21:17:11 +0300

    change how experiment holds conditions

--- src/calcium_imaging/data_models/experiment.py
+++ src/calcium_imaging/data_models/experiment.py
@@ -9,4 +9,5 @@ class Experiment:
     def __init__(self, name: str, conditions_list: List[Condition]) -> None:
         """Holds multiple conditions of the same experiment."""
         self.name = name
-        self.conditions_list = conditions_list
+        self.condition_to_df = {condition.condition_type: condition for condition in conditions_list}
+        self.condition_types = list(self.condition_to_df.keys())
