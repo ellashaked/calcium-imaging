78395463a24ddb087ff9f7a24cd23d1a210071dc
Eldar Abraham <eldar.abraham@gmail.com>
2025-04-22 20:42:45 +0300

    rename utils to discard_first_n_points

--- src/calcium_imaging/preprocessing/__init__.py
+++ src/calcium_imaging/preprocessing/__init__.py
@@ -2,4 +2,4 @@ from .normalize import normalize
 from .smoothen import smoothen
 from .sort_columns import sort_columns
 from .subtract_background_fluorescence import subtract_background_fluorescence
-from .utils import discard_first_n_points
+from .discard_first_n_points import discard_first_n_points
