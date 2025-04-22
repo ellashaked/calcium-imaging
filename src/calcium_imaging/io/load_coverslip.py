095a59171edb574efd9d17af9a79ab286e477ead
Eldar Abraham <eldar.abraham@gmail.com>
2025-04-22 22:46:13 +0300

    supress OLE2 inconsistency warning

--- src/calcium_imaging/io/load_coverslip.py
+++ src/calcium_imaging/io/load_coverslip.py
@@ -1,13 +1,18 @@
+import os
+import warnings
 from pathlib import Path
 from typing import Tuple
 
 import pandas as pd
+import xlrd
 
 RawRun = Tuple[str, pd.DataFrame]
 
 
 def _load_xls_coverslip(xls_path: Path) -> RawRun:
-    return xls_path.stem, pd.read_excel(xls_path)
+    wb = xlrd.open_workbook(xls_path, logfile=open(os.devnull, "w"))  # to supress OLE2 inconsistency warning
+    df = pd.read_excel(wb, engine="xlrd")
+    return xls_path.stem, df
 
 
 def load_coverslip(path: Path) -> RawRun:
