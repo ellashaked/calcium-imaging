4ed648915cc1ce155140714cb8d0557b22cf56bc
Eldar Abraham <eldar.abraham@gmail.com>
2025-04-27 21:59:54 +0300

    Add linear_fit

---
+++ src/calcium_imaging/analysis/__init__.py
@@ -0,0 +1,2 @@
+from .linear_fit import linear_fit
+from .regression_coefficients import RegressionCoefficients1D
---
+++ src/calcium_imaging/analysis/linear_fit.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from numpy.polynomial.polynomial import polyfit
+
+from .regression_coefficients import RegressionCoefficients1D
+
+
+def linear_fit(series: pd.Series, start_idx: int, end_idx: int) -> RegressionCoefficients1D:
+    y = series.iloc[start_idx:end_idx]
+    x = np.arange(len(y))
+    regression_coefficients = RegressionCoefficients1D(*polyfit(x, y, deg=1))
+    return regression_coefficients
