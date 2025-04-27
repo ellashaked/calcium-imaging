49e65feff5ef69a07b399f3a4e0727b3ab5bc8bb
Eldar Abraham <eldar.abraham@gmail.com>
2025-04-27 22:24:32 +0300

    calculate_eflux_rates

--- src/calcium_imaging/data_models/coverslip.py
+++ src/calcium_imaging/data_models/coverslip.py
@@ -1,7 +1,10 @@
+from typing import Dict
+
 from typing_extensions import Self
 
 import pandas as pd
 
+from calcium_imaging.analysis import linear_fit, detect_peak
 from calcium_imaging.constants import BACKGROUND_FLUORESCENCE_ROIS, TIME_COL
 from calcium_imaging.preprocessing_utils import (
     discard_first_n_points,
@@ -23,6 +26,7 @@ class Coverslip:
         self.df = self.raw_df.copy(deep=True)
         self.id = int(self.name.split("-")[0].strip())  # TODO regex
         self.group_type = self.name.split("-")[-1].strip()  # TODO regex
+        self.is_preprocessed = False
 
     def __repr__(self) -> str:
         return str(self.name)
@@ -34,4 +38,18 @@ class Coverslip:
         self.df = self.df.drop(columns=[TIME_COL] + BACKGROUND_FLUORESCENCE_ROIS)
         self.df = normalize(self.df, sampling_start_frame=1, sampling_end_frame=35)
         self.df = rename_columns(self.df, f"cs-{self.id}")
+        self.is_preprocessed = True
         return self
+
+    def calculate_eflux_rates(self) -> Dict[str, float]:
+        if not self.is_preprocessed:
+            raise RuntimeError(f"Cannot calculate eflux for an unprocessed coverslip. "
+                               f"Please call cs.preprocess() before calculating eflux.")
+        roi_to_eflux = dict()
+        for roi_name, series in self.df.iteritems():
+            start_idx = detect_peak(series)
+            end_idx = start_idx + 5  # TODO magic number
+            linear_coefficients = linear_fit(series, start_idx, end_idx)
+            eflux = linear_coefficients.slope
+            roi_to_eflux[str(roi_name)] = eflux
+        return roi_to_eflux
