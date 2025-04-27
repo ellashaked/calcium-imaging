5b5c987ffc28cbdc5db5b06c0cc38d7858e02fbb
Eldar Abraham <eldar.abraham@gmail.com>
2025-04-27 23:29:24 +0300

    INtroduce ROI

--- src/calcium_imaging/data_models/__init__.py
+++ src/calcium_imaging/data_models/__init__.py
@@ -2,3 +2,4 @@ from .coverslip import Coverslip
 from .experiment import Experiment
 from .group import Group
 from .regression_coefficients import RegressionCoefficients1D
+from .roi import ROI
--- src/calcium_imaging/data_models/roi.py
+++ src/calcium_imaging/data_models/roi.py
@@ -6,8 +6,15 @@ from calcium_imaging.analysis import detect_peak, linear_fit
 class ROI:
     """Single region of interest"""
 
-    def __init__(self, name: str, series: pd.Series) -> None:
-        self.name = name
+    def __init__(
+            self,
+            coverslip_id: int,
+            roi_id: int,
+            series: pd.Series
+    ) -> None:
+        self.coverslip_id = coverslip_id
+        self.roi_id = roi_id
+        self.name = f"cs-{self.coverslip_id}_roi-{self.roi_id}"
         self.series = series
 
     def calculate_eflux(self) -> float:  # TODO magic numbers
@@ -16,3 +23,6 @@ class ROI:
         linear_coefficients = linear_fit(self.series, start_idx, end_idx)
         eflux = linear_coefficients.slope
         return eflux
+
+    def __repr__(self) -> str:
+        return self.name
