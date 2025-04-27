30d3f8b7dbeb094721b744076de5ac37e8a9185c
Eldar Abraham <eldar.abraham@gmail.com>
2025-04-28 01:57:38 +0300

    remove more redunant methods

--- src/calcium_imaging/__init__.py
+++ src/calcium_imaging/__init__.py
@@ -2,4 +2,3 @@ from .data_models import *
 from .instantiation import load_experiment
 from .io import *
 from .processing import *
-from .visualization import *
--- src/calcium_imaging/visualization/__init__.py
+++
@@ -1,2 +0,0 @@
-from .plot_peak_highlight import plot_peak_highlight
-from .plot_slope import plot_slope
