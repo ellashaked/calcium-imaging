644d119c374f081db51d400433baae51d4caa058
Eldar Abraham <eldar.abraham@gmail.com>
2025-04-28 01:57:07 +0300

    remove irelevant methods

--- src/calcium_imaging/analysis/__init__.py
+++ src/calcium_imaging/analysis/__init__.py
@@ -1,4 +1,2 @@
-from .calculate_eflux_rate import calculate_eflux_rate
-from .detect_peak import detect_peak
 from .linear_fit import linear_fit
 from .regression_coefficients import RegressionCoefficients1D
--- src/calcium_imaging/analysis/detect_peak.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-
-def detect_peak(series: pd.Series) -> int:
-    return series.argmax()
