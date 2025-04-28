55eae3194d43a8389a98c71a148074fc063b9b9e
Eldar Abraham <eldar.abraham@gmail.com>
2025-04-28 21:49:26 +0300

    update main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from pathlib import Path
 
 from calcium_imaging import load_experiment, Preprocessor, Experiment
 
 
-def visualize_box_plot(df: pd.DataFrame, experiment_name: str) -> None:
+def visualize_eflux_box_plot(df: pd.DataFrame, experiment_name: str) -> None:
     plt.figure()
     plt.boxplot(
         [df.loc[df['group_type'] == g, 'eflux'] for g in df['group_type'].unique()],
@@ -17,6 +18,19 @@ def visualize_box_plot(df: pd.DataFrame, experiment_name: str) -> None:
     plt.show()
 
 
+def visualize_amplitude_box_plot(df: pd.DataFrame, experiment_name: str) -> None:
+    plt.figure()
+    plt.boxplot(
+        [df.loc[df['group_type'] == g, 'amplitude'] for g in df['group_type'].unique()],
+        labels=df['group_type'].unique()
+    )
+    plt.xlabel('Group Type')
+    plt.ylabel('Amplitude')
+    plt.title(f'{experiment_name}\nDistribution of Amplitudes by Group')
+    plt.tight_layout()
+    plt.show()
+
+
 def visualize_all_rois(experiment: Experiment) -> None:
     for group in experiment:
         for coverslip in group:
@@ -25,6 +39,7 @@ def visualize_all_rois(experiment: Experiment) -> None:
 
 
 def main():
+    raw_data_dir = "./raw_data"
     preprocessor = Preprocessor(
         first_n_points_to_discard=5,
         smoothing_windows_size=2,
@@ -36,14 +51,22 @@ def main():
         drop_time_col=True,
         drop_background_fluorescence_cols=True,
     )
-    experiment = load_experiment(
-        experiment_dir="./raw_data/fish_NCLX_long_and_short",
-        preprocessor=preprocessor
-    )
-    # experiment.save_mega_dfs("./results")
-    eflux_rates_df = pd.DataFrame.from_records(experiment.calculate_eflux_rates(return_json=True))
-    visualize_box_plot(df=eflux_rates_df, experiment_name=experiment.name)
-    # visualize_all_rois(experiment)
+    for experiment_dir in Path(raw_data_dir).iterdir():
+        if not experiment_dir.is_dir():
+            continue
+        print("-" * 50)
+        print(experiment_dir.stem)
+        print("-" * 50)
+        experiment = load_experiment(experiment_dir=experiment_dir, preprocessor=preprocessor)
+
+        # experiment.save_mega_dfs("./results")
+        eflux_rates_df = pd.DataFrame.from_records(experiment.calculate_eflux_rates(return_json=True))
+        visualize_eflux_box_plot(df=eflux_rates_df, experiment_name=experiment.name)
+
+        amplitudes_df = pd.DataFrame.from_records(experiment.calculate_amplitudes(return_json=True))
+        visualize_amplitude_box_plot(df=amplitudes_df, experiment_name=experiment.name)
+
+        # visualize_all_rois(experiment)
 
     print()
 
