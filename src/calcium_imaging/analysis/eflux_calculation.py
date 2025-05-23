ac8076669c66925427df7c09962f13e71875512c
Eldar Abraham <eldar.abraham@gmail.com>
2025-05-23 11:50:56 +0300

    fix end_indx bug

--- src/calcium_imaging/analysis/eflux_calculation.py
+++ src/calcium_imaging/analysis/eflux_calculation.py
@@ -16,7 +16,8 @@ def calculate_eflux_linear_coefficients(
         start_idx: Optional[int] = None,
         end_idx: Optional[int] = None
 ) -> RegressionCoefficients1D:
-    start_idx = start_idx if start_idx is not None else detect_eflux_start_index(trace, )
+    start_idx = start_idx if start_idx is not None else detect_eflux_start_index(trace)
+    end_idx = end_idx if end_idx is not None else detect_eflux_end_index(trace)
     if end_idx <= start_idx:
         raise RuntimeError(f"error calculating eflux for trace '{trace.name}', end_idx <= start_idx")
     linear_coefficients = linear_fit(trace, start_idx, end_idx)
--- src/calcium_imaging/analysis/influx_calculation.py
+++ src/calcium_imaging/analysis/influx_calculation.py
@@ -17,6 +17,7 @@ def calculate_influx_linear_coefficients(
         end_idx: Optional[int] = None
 ) -> RegressionCoefficients1D:
     start_idx = start_idx if start_idx is not None else detect_influx_start_index(trace)
+    end_idx = end_idx if end_idx is not None else detect_influx_end_index(trace)
     if end_idx <= start_idx:
         raise RuntimeError(f"error calculating influx for trace '{trace.name}', end_idx <= start_idx")
     linear_coefficients = linear_fit(trace, start_idx, end_idx)
