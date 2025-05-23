402f7a75146579d19f061db59f1ef369b17239fa
Eldar Abraham <eldar.abraham@gmail.com>
2025-05-23 12:33:56 +0300

    remove auto inferring start and end indexes from in/eflux calc

--- src/calcium_imaging/analysis/eflux_calculation.py
+++ src/calcium_imaging/analysis/eflux_calculation.py
@@ -2,8 +2,8 @@ from typing import Optional
 
 import pandas as pd
 
-from .peak_detection import detect_peak_index
 from .linear_fit import linear_fit
+from .peak_detection import detect_peak_index
 from .regression_coefficients import RegressionCoefficients1D
 
 EFLUX_START_INDEX_OFFSET_FROM_PEAK = 5
@@ -16,8 +16,6 @@ def calculate_eflux_linear_coefficients(
         start_idx: Optional[int] = None,
         end_idx: Optional[int] = None
 ) -> RegressionCoefficients1D:
-    start_idx = start_idx if start_idx is not None else detect_eflux_start_index(trace)
-    end_idx = end_idx if end_idx is not None else detect_eflux_end_index(trace)
     if end_idx <= start_idx:
         raise RuntimeError(f"error calculating eflux for trace '{trace.name}', end_idx <= start_idx")
     linear_coefficients = linear_fit(trace, start_idx, end_idx)
--- src/calcium_imaging/analysis/influx_calculation.py
+++ src/calcium_imaging/analysis/influx_calculation.py
@@ -2,8 +2,6 @@ from typing import Optional
 
 import pandas as pd
 
-from .peak_detection import detect_peak_index
-from .onset_detection import detect_onset_index
 from .linear_fit import linear_fit
 from .regression_coefficients import RegressionCoefficients1D
 
@@ -17,8 +15,6 @@ def calculate_influx_linear_coefficients(
         start_idx: Optional[int] = None,
         end_idx: Optional[int] = None
 ) -> RegressionCoefficients1D:
-    start_idx = start_idx if start_idx is not None else detect_onset_index(trace)
-    end_idx = end_idx if end_idx is not None else detect_peak_index(trace) - INFLUX_END_INDEX_OFFSET_FROM_PEAK
     if end_idx <= start_idx:
         raise RuntimeError(f"error calculating influx for trace '{trace.name}', end_idx <= start_idx")
     linear_coefficients = linear_fit(trace, start_idx, end_idx)
